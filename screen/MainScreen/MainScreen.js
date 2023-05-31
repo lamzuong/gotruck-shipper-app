92e7ebceec84046f710c70088e9c469832fba566
QuocNguyen219 <quoc21921@gmail.com>
2023-05-31 15:03:07 +0700

    alert when account blocked

--- screen/MainScreen/MainScreen.js
+++ screen/MainScreen/MainScreen.js
@@ -1,36 +1,57 @@
-import { publicRoutes } from "./routes/routes";
+import { publicRoutes } from './routes/routes';
 
-import React, { useEffect } from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Alert, BackHandler } from "react-native";
+import React, { useContext, useEffect } from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Alert, BackHandler } from 'react-native';
+import { socketClient } from '../../global/socket';
+import { AuthContext } from '../../context/AuthContext';
+import { LoginSuccess } from '../../context/AuthAction';
+import axiosClient from '../../api/axiosClient';
 const Tab = createBottomTabNavigator();
 
 export default function MainScreen() {
+  const { dispatch, user } = useContext(AuthContext);
+
   //----------Back Button----------
   useEffect(() => {
     const backAction = () => {
-      Alert.alert("Thông báo", "Bạn có muốn thoát ứng dụng không ?", [
+      Alert.alert('Thông báo', 'Bạn có muốn thoát ứng dụng không ?', [
         {
-          text: "Không",
+          text: 'Không',
           onPress: () => null,
-          style: "cancel",
+          style: 'cancel',
         },
-        { text: "Có", onPress: () => BackHandler.exitApp() },
+        { text: 'Có', onPress: () => BackHandler.exitApp() },
       ]);
       return true;
     };
-    const backHandler = BackHandler.addEventListener(
-      "hardwareBackPress",
-      backAction
-    );
+    const backHandler = BackHandler.addEventListener('hardwareBackPress', backAction);
     return () => backHandler.remove();
   }, []);
   //------------------------------
+  useEffect(() => {
+    socketClient.off('block_account');
+    socketClient.on('block_account', async (data) => {
+      if (user._id === data.id_receive) {
+        if (!user.block) {
+          Alert.alert(
+            'Thông báo',
+            'Tài khoản bạn đã bị khóa và không thể nhận đơn hàng tiếp theo',
+          );
+        } else {
+          Alert.alert('Thông báo', 'Tài khoản của bạn đã được mở khóa');
+        }
+        const userLogin = await axiosClient.get('/gotruck/authshipper/user/' + user.phone);
+        dispatch(LoginSuccess(userLogin));
+      }
+    });
+  });
+
   return (
     <Tab.Navigator
       screenOptions={{
-        tabBarActiveBackgroundColor: "white",
-        tabBarInactiveBackgroundColor: "white",
+        tabBarActiveBackgroundColor: 'white',
+        tabBarInactiveBackgroundColor: 'white',
       }}
     >
       {publicRoutes.map((route, index) => {
@@ -43,8 +64,7 @@ export default function MainScreen() {
               headerShown: route.header,
               tabBarLabel: () => null,
               tabBarHideOnKeyboard: true,
-              tabBarIcon: ({ focused }) =>
-                focused ? route.iconActive : route.iconInactive,
+              tabBarIcon: ({ focused }) => (focused ? route.iconActive : route.iconInactive),
             }}
           />
         );
